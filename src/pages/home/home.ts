58fd2cd53abf25a0724a87e4de72bd6122afc449
Mattia La Spina <mattiaa95@gmail.com>
2019-01-16 18:51:56 +0000

    tts stable funcional

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,34 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import {TextToSpeech} from '@ionic-native/text-to-speech';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+text: string;
+rate: number;
+locale: string;
+@ViewChild('myInput') myInput: ElementRef;
 
-  constructor(public navCtrl: NavController) {
+constructor(private tts: TextToSpeech,) {
+  this.text = '';
+  this.rate = 1;
+  this.locale = 'en-US';
+}
 
-  }
+playText() {
+  this.tts.speak({
+    text: this.text,
+    rate: this.rate / 10,
+    locale: this.locale
+  })
+    .then(() => console.log('Success'))
+    .catch((reason: any) => console.log(reason));
+}
+
+resize() {
+    this.myInput.nativeElement.style.height = this.myInput.nativeElement.scrollHeight + 'px';
+}
 
 }
